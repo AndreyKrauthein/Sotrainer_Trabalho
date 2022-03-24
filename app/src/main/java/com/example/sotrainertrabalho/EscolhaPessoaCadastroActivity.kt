3171c5eeedb45bdd3b14f978724b504d4d9da00c
AndreyKrauthein <54869693+AndreyKrauthein@users.noreply.github.com>
2022-03-24 10:17:32 -0300

    second commit

--- app/src/main/java/com/example/sotrainertrabalho/EscolhaPessoaCadastroActivity.kt
+++ app/src/main/java/com/example/sotrainertrabalho/EscolhaPessoaCadastroActivity.kt
@@ -1,11 +1,23 @@
 package com.example.sotrainertrabalho
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class EscolhaPessoaCadastroActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_escolha_pessoa_cadastro)
+
+        findViewById<Button>(R.id.academia).setOnClickListener {
+            val intent = Intent(this, CadastroAcademiaActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.aluno).setOnClickListener {
+            val intent = Intent(this, CadastroAlunoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
