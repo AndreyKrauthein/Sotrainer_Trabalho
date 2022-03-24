33507e1c469791c13fcf9efd84795b5b2698b9c2
AndreyKrauthein <54869693+AndreyKrauthein@users.noreply.github.com>
2022-03-24 09:47:19 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sotrainertrabalho/EscolhaEntradaActivity.kt
@@ -0,0 +1,18 @@
+package com.example.sotrainertrabalho
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class EscolhaEntradaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_escolha_entrada)
+
+        findViewById<Button>(R.id.login).setOnClickListener {
+            val intent = Intent(this, EscolhaPessoaLoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sotrainertrabalho/EscolhaPessoaLoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sotrainertrabalho
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class EscolhaPessoaLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_escolha_pessoa_login)
+
+    }
+}
